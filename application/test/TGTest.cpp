026cf9fdbfc993ececc4ef7bdb15bf14ea31fc33
MrTroble <nifroh56@gmail.com>
2022-12-23 15:51:15 +0100

    test: update single testing

--- application/test/TGTest.cpp
+++ application/test/TGTest.cpp
@@ -1,24 +1,34 @@
 #define TGE_IMPORT_INTEROP 1
 #include <TGEngine.hpp>
-#include "../TGApp.hpp"
 #include <thread>
+
+#include "../TGApp.hpp"
 
 using namespace tge::main;
 using namespace tge::graphics;
 
 void test() {
-	waitFinishedInit();
-	ReferenceLoad load;
-	load.formKey = (FormKey)"TEST";
-	load.path = (char*)"wrhouse02.nif";
-	load.transform = TGE_DEFAULT_TRANSFORM;
-	const auto ref = loadReferences(1, &load);
-	printf("Loaded, %d\n", ref);
+  waitFinishedInit();
+  std::vector<ReferenceLoad> loads;
+  std::vector<std::string> names(100);
+  std::fill(begin(names), end(names), "wrhouse02.nif");
+  for (const auto& name : names) {
+    ReferenceLoad load;
+    load.formKey = (FormKey) "TEST";
+    load.path = name.c_str();
+    load.transform = TGE_DEFAULT_TRANSFORM;
+    loads.push_back(load);
+  }
+  const auto ref = loadReferences(loads.size(), loads.data());
+  printf("Loaded, %d\n", ref);
+
+  const auto ref2 = loadReferences(loads.size(), loads.data());
+  printf("Loaded, %d\n", ref2);
 }
 
 int main(int argv, const char** in) {
-	std::thread thread(&test);
-	thread.detach();
-	const InitConfig config{ 1, "assets" };
-	return initTGEditor(&config);
+  std::thread thread(&test);
+  thread.detach();
+  const InitConfig config{1, "assets"};
+  return initTGEditor(&config);
 }
