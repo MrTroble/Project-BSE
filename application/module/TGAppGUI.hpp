6d7b7dd43e246c71938069b67505a322a590c691
Nico Froehlich <nicofroehlich@icloud.com>
2023-02-04 23:04:33 +0100

    dbg: added debug string

--- application/module/TGAppGUI.hpp
+++ application/module/TGAppGUI.hpp
@@ -1,36 +1,39 @@
 #pragma once
 
+#include <imgui.h>
+
 #include <algorithm>
 #include <graphics/APILayer.hpp>
 #include <graphics/GUIModule.hpp>
 #include <graphics/GameGraphicsModule.hpp>
-#include <imgui.h>
+#include <graphics/PerformanceTestAPI.hpp>
 
 class TGAppGUI : public tge::gui::GUIModule {
-public:
-	tge::graphics::Light light;
-	tge::graphics::APILayer* api;
-	tge::graphics::GameGraphicsModule* ggm;
-	tge::graphics::NodeTransform transformData;
-	size_t lightID;
-	bool focused = false;
+ public:
+  tge::graphics::Light light;
+  tge::graphics::APILayer* api = nullptr;
+  tge::graphics::GameGraphicsModule* ggm;
+  tge::graphics::NodeTransform transformData;
+  size_t lightID;
+  bool focused = false;
 
-	void renderGUI() override {
-		if (ImGui::Begin("test")) {
-			
-			if (
-				ImGui::SliderFloat3("Light", (float*) &light.pos, -10.0f, 10.0f) || //
-				ImGui::SliderFloat("Intesity", (float*)&light.intensity, -10.0f, 10.0f)  //
-				) {
-				lightID = api->pushLights(1, &light);
-			}
-		}
-		focused = ImGui::IsWindowFocused(ImGuiFocusedFlags_AnyWindow);
-		ImGui::End();
-	}
+  void renderGUI() override {
+    if (ImGui::Begin("test")) {
+      if (ImGui::SliderFloat3("Light", (float*)&light.pos, -10.0f, 10.0f) ||  //
+          ImGui::SliderFloat("Intesity", (float*)&light.intensity, -10.0f,
+                             10.0f)  //
+      ) {
+        lightID = api->pushLights(1, &light);
+      }
+      if (api != nullptr) {
+        const auto debug =
+            ((tge::graphics::PerformanceMessuringAPILayer*)api)->getDebug();
+        ImGui::Text(debug.c_str());
+      }
+    }
+    focused = ImGui::IsWindowFocused(ImGuiFocusedFlags_AnyWindow);
+    ImGui::End();
+  }
 
-	void recreate() override {
-		tge::gui::GUIModule::recreate();
-	}
-
+  void recreate() override { tge::gui::GUIModule::recreate(); }
 };
